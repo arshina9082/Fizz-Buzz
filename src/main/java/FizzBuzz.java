56d5591eec19db53e545aa574a50eded44dfcf8b
ARSHINA HANIF DANGE <arshina775@gmail.com>
2021-08-19 23:28:42 +0530

    Req1: To Check if Number is Divisible by 3 or 5

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,19 @@
 public class FizzBuzz {
+    int number;
+
+    public FizzBuzz(int number) {
+        this.number = number;
+    }
+
+    public String divisibleByThreeAndFive() {
+        if (number <= 100) {
+            if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else
+                return "null";
+        }
+        return "null";
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,35 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class FizzBuzzTest {
+    @Test
+    void toCheckIfNumberIsDivisibleByThree()
+    {
+        FizzBuzz obj = new FizzBuzz(6);
+        String expectedOut = "Fizz";
+        String actualOut = obj.divisibleByThreeAndFive();
+        assertEquals(expectedOut,actualOut);
+
+    }
+
+    @Test
+    void toCheckIfNumberIsDivisibleByFive()
+    {
+        FizzBuzz obj = new FizzBuzz(10);
+        String expectedOut = "Buzz";
+        String actualOut = obj.divisibleByThreeAndFive();
+        assertEquals(expectedOut,actualOut);
+
+    }
+
+    @Test
+    void toCheckIfNumberIsDivisibleByNeitherThreeNorFive()
+    {
+        FizzBuzz obj = new FizzBuzz(61);
+        String expectedOut = "null";
+        String actualOut = obj.divisibleByThreeAndFive();
+        assertEquals(expectedOut,actualOut);
+
+    }
 }
